499fc22aec79da6e18b660e2ba897b4118658d4f
rickfleharty <rickfleharty@gmail.com>
2015-11-14 16:38:08 -0700

    Update my_code2.js

--- my_code2.js
+++ my_code2.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
+	
+	var imageName = ["img/bulbON.png", "img/bulbOFF.png"];
+    var indexNum = 1;
+	
+	
   $("#testbutton2").click(function() {
-    $("p").css({
+	//$("#testbutton2").css("background-color","transparent"); 
+	//$("p1").css("color": "yellow");   
+    $("strong").text("off");	
+    $("strong,p1,h1").css({
       "text-align": "center",
-      "color": "blue"
-    });
+      "color": "Red"});
+	
+	
+	$("#picture").fadeOut(50, function() {
+      $("#picture").attr("src", imageName[indexNum]);
+	  //$("#picture").attr("src", imageName[0]);
+      indexNum++;	
+      if (indexNum > 1) {indexNum = 1;}
+      $("#picture").fadeIn(50);
+	});
   });
 });
